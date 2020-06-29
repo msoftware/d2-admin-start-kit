140192876dae38b1382fa259e423f9f8c647eef7
Michael <jentsch@w3logistics.de>
2020-06-29 16:44:19 +0200

    Commit changed files

--- src/views/demo/page1/index.vue
+++ src/views/demo/page1/index.vue
@@ -1,7 +1,9 @@
 <template>
   <d2-container>
     <template slot="header">Page 1 header</template>
-    Hello World
+    <h3>Hello World</h3>
+    <p>{{ $t('message') }}</p>
+	<p v-html="$t('message')"></p>
   </d2-container>
 </template>
 
--- vue.config.js
+++ vue.config.js
@@ -157,7 +157,7 @@ module.exports = {
   // i18n
   pluginOptions: {
     i18n: {
-      locale: 'zh-chs',
+      locale: 'de',
       fallbackLocale: 'en',
       localeDir: 'locales',
       enableInSFC: true
